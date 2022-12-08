ce3268f2c368998fd347a99a195997a5cac1da5a
Hanna Kraska <hanna.kraska11@gmail.com>
2022-12-08 02:44:32 +0100

    fix linting

--- pages/register.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { RegisterForm } from "../src/components/Authentication/RegisterForm";
-export default function RegisterPage() {
-  return (
-    <div>
-      <RegisterForm />
-    </div>
-  );
-}
